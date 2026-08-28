d88edb643214418d50cb9d25eb25528649822fac
Avinaash Reddy Nallamilli <nallamilliavinaashreddy6666@gmail.com>
2026-08-28 15:11:30 +0530

    Completely remove Day Book feature, page route, actions, types, and navigation

--- src/config/navigation.ts
+++ src/config/navigation.ts
@@ -38,12 +38,6 @@ export const navigationItems: NavItem[] = [
     translationKey: 'nav.collections',
   },
   {
-    title: 'Day Book',
-    href: '/day-book',
-    icon: 'BookOpen',
-    translationKey: 'nav.dayBook',
-  },
-  {
     title: 'Interest',
     href: '/interest',
     icon: 'Percent',
